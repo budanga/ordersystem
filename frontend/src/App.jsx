d07682f9ba59dbb3fcb8823fb16716c41311bd48
Lautaro Budin <lautaro_budin@hotmail.com>
2026-03-05 18:31:50 -0300

    Setup global styles and main app entry point

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Layout } from './components/Layout'
+import { Catalog } from './pages/Catalog'
+
+function App() {
+  return (
+    <Layout>
+      <Catalog />
+    </Layout>
+  )
+}
+
+export default App
