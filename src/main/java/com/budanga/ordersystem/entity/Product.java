d0ef63a37e46487b49d34c704029da61a20497fc
Lautaro Budin <lautaro_budin@hotmail.com>
2026-03-05 11:13:18 -0300

    Delete stash text

--- src/main/java/com/budanga/ordersystem/entity/Product.java
+++ src/main/java/com/budanga/ordersystem/entity/Product.java
@@ -6,26 +6,16 @@ import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
->>>>>>> Stashed changes
 
 @Entity
 @Table(name = "products")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-<<<<<<< Updated upstream
-=======
 @EntityListeners(AuditingEntityListener.class)
->>>>>>> Stashed changes
 public class Product {
 
     @Id
@@ -48,14 +38,7 @@ public class Product {
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-<<<<<<< Updated upstream
-    @PrePersist
-    public void prePersist() {
-        this.createdAt = LocalDateTime.now();
-    }
-=======
     @LastModifiedDate
     @Column(nullable = false)
     private LocalDateTime updatedAt;
->>>>>>> Stashed changes
 }
